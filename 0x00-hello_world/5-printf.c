17c49a74d59e21f0232fb93738e1035f089cdd52
TAYO389 <omotayooluwole389@gmail.com>
2023-02-02 01:29:59 -0800

    task 8

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - main block
- *
+ * main - prints the phrase with proper grammar,
  * use: printf to print the string of ("%s", "with proper grammar, but the outcome is a piece of art,\n")
- *
  * return 0
  */
-
 int main(void)
 {
 	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
